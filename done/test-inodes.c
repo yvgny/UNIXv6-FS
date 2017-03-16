855c849d2c3a0ed490d10efa187f56ce75c3e812
Sacha Kozma <sacha.kozma@gmail.com>
2017-03-16 14:45:04 +0100

    Initial commit

---
+++ done/test-inodes.c
@@ -0,0 +1,13 @@
+/**
+ * @file test-inodes.c
+ * @brief Implement a test function for the inodes
+ *
+ * @author Théo Nikles & Sacha Kozma
+ * @date 16 mars 2017
+ *
+ */
+#include "mount.h"
+
+int test(struct unix_filesystem *u) {
+    return 0;
+}
