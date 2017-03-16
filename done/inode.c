298540e31cc5d0a91b2471557aefa60c36e87a31
Sacha Kozma <sacha.kozma@gmail.com>
2017-03-16 17:43:39 +0100

    Initial file created

---
+++ done/inode.c
@@ -0,0 +1,14 @@
+/**
+ * @file  inode.c
+ * @brief accessing the UNIX v6 filesystem -- core of the first set of assignments.
+ *
+ * @author Théo Nikles et Sacha Kozma
+ * @date 16 mars 2017
+ */
+
+#include "error.h"
+
+int inode_scan_print(const struct unix_filesystem *u) {
+    M_REQUIRE_NON_NULL(u);
+
+}
