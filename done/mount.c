29f2f7de2d5152515f7baa0d329025719314388a
Sacha Kozma <sacha.kozma@gmail.com>
2017-03-16 15:05:32 +0100

    Added mount.c

---
+++ done/mount.c
@@ -0,0 +1,23 @@
+/**
+ * @file mount.c
+ * @brief Used to mount the filesystem
+ *
+ * @author Théo Nikles & Sacha Kozma
+ * @date 16 mars 2017
+ */
+#include <stdio.h>
+
+int mountv6(const char *filename, struct unix_filesystem *u)
+{
+	return 0;
+}
+
+void mountv6_print_superblock(const struct unix_filesystem *u)
+{
+	
+}
+
+int umountv6(struct unix_filesystem *u)
+{
+	return 0;
+}
