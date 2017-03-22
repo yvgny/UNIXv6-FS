25ef99c012a41b4f5bc9d471b2c449ee61ac17aa
Sacha Kozma <sacha.kozma@gmail.com>
2017-03-22 14:03:26 +0100

    Added alternative function form readblock. TODO: check the right one.

--- done/filev6.c
+++ done/filev6.c
@@ -4,26 +4,50 @@
 #include "filev6.h"
 
 int filev6_open(const struct unix_filesystem *u, uint16_t inr, struct filev6 *fv6) {
-	struct inode* inode;
-	int error = inode_read(u, inr, inode);
-	if (error != 0) return error;
-	fv6->u = u;
-	fv6->i_number = inr;
-	fv6->i_node = *inode;
-	fv6->offset = 0;
-	
-	return 0;
+    struct inode *inode;
+    int error = inode_read(u, inr, inode);
+    if (error != 0) {
+        return error;
+    }
+    fv6->u = u;
+    fv6->i_number = inr;
+    fv6->i_node = *inode;
+    fv6->offset = 0;
+
+    return 0;
 }
 
 int filev6_readblock(struct filev6 *fv6, void *buf) {
-	if(fv6->offset % SECTOR_SIZE != 0) {
-		fv6->offset = 0;
-		return inode_getsize(&(fv6->i_node));
-	}
-	int error = sector_read(fv6->u->f, inode_findsector(fv6->u, &(fv6->i_node), fv6->offset), buf);
-	if (error != 0) return error;
-	fv6->offset += SECTOR_SIZE;
-	return 0;
+    /*if(fv6->offset % SECTOR_SIZE != 0) {
+        fv6->offset = 0;
+        return inode_getsize(&(fv6->i_node));
+    }
+    int error = sector_read(fv6->u->f, inode_findsector(fv6->u, &(fv6->i_node), fv6->offset), buf);
+    if (error != 0) return error;
+    fv6->offset += SECTOR_SIZE;
+    return 0;
+     */
+    M_REQUIRE_NON_NULL(fv6);
+    M_REQUIRE_NON_NULL(buf);
+
+    if (fv6->offset >= inode_getsize(&(fv6->i_node))) {
+        return 0;
+    }
+
+    int error = sector_read(fv6->u->f, inode_findsector(fv6->u, &(fv6->i_node), fv6->offset / SECTOR_SIZE), buf);
+    if (error != 0) {
+        return error;
+    }
+    int byteRead = 0;
+    int remainingByte = 0;
+    if (remainingByte = inode_getsize(&(fv6->i_node)) - fv6->offset > SECTOR_SIZE) {
+        byteRead = SECTOR_SIZE;
+    } else {
+        byteRead = remainingByte;
+    }
+    fv6->offset += byteRead;
+
+    return byteRead;
 }
 
 
