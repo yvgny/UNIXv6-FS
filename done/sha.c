10c73aae683d365e23949daec143adeab25284c9
Sacha Kozma <sacha.kozma@gmail.com>
2017-03-22 11:05:50 +0100

    print_sha_inode finished

--- done/sha.c
+++ done/sha.c
@@ -7,20 +7,19 @@
 
 
 void print_sha(unsigned char sha[]) {
-	if(sha == NULL) {
-		return;
-	}
-	for(int i = 0 ; i < SHA256_DIGEST_LENGTH ; i++)
-		printf("%02x", sha[i]);
-	printf("\n");
+    if (sha == NULL) {
+        return;
+    }
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+        printf("%02x", sha[i]);
+    printf("\n");
 }
 
-void print_sha_from_content(const unsigned char* content, size_t length) {
-	print_sha(SHA256(content, length, NULL));
+void print_sha_from_content(const unsigned char *content, size_t length) {
+    print_sha(SHA256(content, length, NULL));
 }
 
-static void sha_to_string(const unsigned char *SHA, char *sha_string)
-{
+static void sha_to_string(const unsigned char *SHA, char *sha_string) {
     if ((SHA == NULL) || (sha_string == NULL)) {
         return;
     }
@@ -32,18 +31,19 @@ static void sha_to_string(const unsigned char *SHA, char *sha_string)
 }
 
 void print_sha_inode(struct unix_filesystem *u, struct inode inode, int inr) {
-	if(inode.i_mode != IALLOC) {
-		return;
-	} else {
-		printf("SHA inode %d: ", inr);
-	}
-	if(inode.i_mode & IFDIR) {
-		printf("no SHA for directories.\n");
-	} else {
-		unsigned char content[CONTENT_MAX_SIZE];
-		for(int i = 0 ; i < inode_getsize(&inode) ; i++) {
-			sector_read(u->f, inode_findsector(u, &inode, inode_getsize(&inode) / SECTOR_SIZE), content + i * SECTOR_SIZE);
-		}
-		print_sha_from_content(content, CONTENT_MAX_SIZE);
-	}
+    if (u == NULL || u->f == NULL || inode.i_mode != IALLOC) {
+        return;
+    }
+
+    printf("SHA inode %d: ", inr);
+
+    if (inode.i_mode & IFDIR) {
+        printf("no SHA for directories.\n");
+    } else {
+        unsigned char content[inode_getsectorsize(&inode)];
+        for (int i = 0; i < inode_getsectorsize(&inode); i++) {
+            sector_read(u->f, inode_findsector(u, &inode, i), &content[i * SECTOR_SIZE]);
+        }
+        print_sha_from_content(content, CONTENT_MAX_SIZE);
+    }
 }
