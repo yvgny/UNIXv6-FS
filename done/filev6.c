eeaf4ed39b4dfe80130241ddc242370414e4c695
Sacha Kozma <sacha.kozma@gmail.com>
2017-03-26 13:26:02 +0200

    Modifications done in filev6

--- done/filev6.c
+++ done/filev6.c
@@ -36,15 +36,15 @@ int filev6_readblock(struct filev6 *fv6, void *buf) {
     M_REQUIRE_NON_NULL(buf);
     M_REQUIRE_NON_NULL(fv6->u);
 	
-    if (fv6->offset >= inode_getsize(&(fv6->i_node)) /*|| fv6->offset < 0*/) {
+    if (fv6->offset >= inode_getsize(&(fv6->i_node)) || fv6->offset < 0) {
         return 0;
     }
     
-    /*int sector = inode_findsector(fv6->u, &(fv6->i_node);
-    if(error == ERR_UNALLOCATED_INODE || error == ERR_OFFSET_OUT_OF_RANG) {
+    int sector = inode_findsector(fv6->u, &(fv6->i_node));
+    if(sector < 0) {
 		return sector;
-	}*/
-    int error = sector_read(fv6->u->f, /*sector*/inode_findsector(fv6->u, &(fv6->i_node), fv6->offset / SECTOR_SIZE), buf);
+	}
+    int error = sector_read(fv6->u->f, sector, fv6->offset / SECTOR_SIZE), buf);
 
     if (error) {
         return error;
