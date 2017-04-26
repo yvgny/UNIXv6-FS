ec7312f90a1045fcee10d6b1ddc74d45b4c4c2ea
Sacha Kozma <sacha.kozma@gmail.com>
2017-04-26 10:32:30 +0200

    First version of fs_getaddr

--- done/fs.c
+++ done/fs.c
@@ -18,73 +18,102 @@
 #include <stdlib.h>
 #include "error.h"
 #include "mount.h"
+#include "direntv6.h"
+#include "inode.h"
 
 struct unix_filesystem fs;
 
-static int arg_parse(void*, const char*, int, struct fuse_args*);
+void print_error(int);
+
+static int arg_parse(void *, const char *, int, struct fuse_args *);
+
 static int fs_getattr(const char *path, struct stat *stbuf);
+
 static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-			 off_t offset, struct fuse_file_info *fi);
+                      off_t offset, struct fuse_file_info *fi);
+
 static int fs_read(const char *path, char *buf, size_t size, off_t offset,
-		      struct fuse_file_info *fi);
+                   struct fuse_file_info *fi);
 
 static struct fuse_operations available_ops = {
-	.getattr	= fs_getattr,
-	.readdir	= fs_readdir,
-	.read		= fs_read,
+        .getattr    = fs_getattr,
+        .readdir    = fs_readdir,
+        .read        = fs_read,
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
     int ret = fuse_opt_parse(&args, NULL, NULL, arg_parse);
     if (ret == 0) {
         ret = fuse_main(args.argc, args.argv, &available_ops, NULL);
-        (void)umountv6(&fs);
+        (void) umountv6(&fs);
     }
     return ret;
 }
 
-static int arg_parse(void *data, const char *filename, int key, struct fuse_args *outargs)
-{
+static int arg_parse(void *data, const char *filename, int key, struct fuse_args *outargs) {
     (void) data;
     (void) outargs;
     if (key == FUSE_OPT_KEY_NONOPT && fs.f == NULL && filename != NULL) {
-		int error = mountv6(filename, &fs);
-		if (error < 0) {
-			umountv6(&fs);
-			fprintf(stderr, "ERROR FS: %s.\n", ERR_MESSAGES[error - ERR_FIRST]);
-			exit(1);
-		}
+        int error = mountv6(filename, &fs);
+        if (error < 0) {
+            umountv6(&fs);
+            print_error(error);
+            exit(1);
+        }
         return 0;
     }
     return 1;
 }
 
-static int fs_getattr(const char *path, struct stat *stbuf)
-{
-	int res = 0;
+static int fs_getattr(const char *path, struct stat *stbuf) {
+    int res = 0, inr = 0;
+    memset(stbuf, 0, sizeof(struct stat));
 
-	memset(stbuf, 0, sizeof(struct stat));
+    inr = direntv6_dirlookup(&fs, ROOT_INUMBER, path);
+    if (inr < 0) {
+        print_error(inr);
+        return inr;
+    }
 
-	return res;
+    struct inode i_node;
+    res = inode_read(&fs, inr, &i_node);
+    if (res < 0) {
+        print_error(res);
+        return res;
+    }
+
+    stbuf->st_ino = inr;
+    stbuf->st_mode = S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH;
+    stbuf->st_mode = stbuf->st_mode | (i_node.i_mode & IFDIR ? S_IFDIR : S_IFREG);
+    stbuf->st_uid = i_node.i_uid;
+    stbuf->st_gid = i_node.i_gid;
+    stbuf->st_gid = inode_getsize(&i_node);
+    stbuf->st_blksize = SECTOR_SIZE;
+    stbuf->st_blocks = inode_getsectorsize(&i_node);
+
+    return 0;
 }
 
 static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-			 off_t offset, struct fuse_file_info *fi)
-{
-	(void) offset;
-	(void) fi;
+                      off_t offset, struct fuse_file_info *fi) {
+    (void) offset;
+    (void) fi;
 
-	filler(buf, ".", NULL, 0);
-	filler(buf, "..", NULL, 0);
+    filler(buf, ".", NULL, 0);
+    filler(buf, "..", NULL, 0);
 
-	return 0;
+    return 0;
 }
 
 static int fs_read(const char *path, char *buf, size_t size, off_t offset,
-		      struct fuse_file_info *fi)
-{
-	(void) fi;
+                   struct fuse_file_info *fi) {
+    (void) fi;
 
-	return size;
+    return size;
+}
+
+void print_error(int error) {
+    fprintf(stderr, "ERROR FS: %s.\n", ERR_MESSAGES[error - ERR_FIRST]);
+    return;
 }
