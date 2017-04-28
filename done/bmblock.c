a0a6b7cf1b0207a20b73f6b684fd53740912e3b3
Theo <theo.nikles@epfl.ch>
2017-04-28 11:12:10 +0200

    bmblock must be corrected to handle relative position

--- done/bmblock.c
+++ done/bmblock.c
@@ -8,6 +8,8 @@
 
 #define BM_MEMBER_SIZE 64
 
+void print_binary(uint64_t);
+
 struct bmblock_array *bm_alloc(uint64_t min, uint64_t max) {
 	if(min > max || min < 0) {
 		return NULL;
@@ -19,48 +21,79 @@ struct bmblock_array *bm_alloc(uint64_t min, uint64_t max) {
 	}
 	memset(bmblock, 0, sizeof(*bmblock));
 	bmblock->length = length;
-	bmblock->min = min;
+	bmblock->min = bmblock->cursor = min;
 	bmblock->max = max;
 	
 	return bmblock;
+}
+
+void bm_free(struct bmblock_array *bmblock) {
+	free(bmblock);
 }
 
 int bm_get(struct bmblock_array *bmblock_array, uint64_t x) {
 	if (bmblock_array->min > x || bmblock_array->max < x) {
 		return ERR_BAD_PARAMETER;
 	}
-	return (bmblock_array->bm[x / BM_MEMBER_SIZE] >> x % BM_MEMBER_SIZE) & 1;
+	uint64_t relative_x = x - bmblock_array->min;
+	return (bmblock_array->bm[relative_x / BM_MEMBER_SIZE] >> relative_x % BM_MEMBER_SIZE) & 1;
 }
 
-/**
- * @brief set to true (or 1) the bit associated to the given value
- * @param bmblock_array the array containing the value we want to set
- * @param x an integer corresponding to the number of the value we are looking for
- */
 void bm_set(struct bmblock_array *bmblock_array, uint64_t x) {
-	
+	if (bmblock_array->min > x || bmblock_array->max < x) {
+		return;
+	}
+	uint64_t relative_x = x - bmblock_array->min;
+	uint64_t mask = 1 << ((relative_x - bmblock_array->min) % BM_MEMBER_SIZE);
+	bmblock_array->bm[relative_x / BM_MEMBER_SIZE] |= mask;
 }
 
-/**
- * @brief set to false (or 0) the bit associated to the given value
- * @param bmblock_array the array containing the value we want to clear
- * @param x an integer corresponding to the number of the value we are looking for
- */
-void bm_clear(struct bmblock_array *bmblock_array, uint64_t x);
-
-/**
- * @brief return the next unused bit
- * @param bmblock_array the array we want to search for place
- * @return <0 on failure, the value of the next unused value otherwise
- */
-int bm_find_next(struct bmblock_array *bmblock_array);
-
-/**
- * @brief usefull to see (and debug) content of a bmblock_array
- * @param bmblock_array the array we want to see
- */
-void bm_print(struct bmblock_array *bmblock_array);
-
-#ifdef __cplusplus
+void bm_clear(struct bmblock_array *bmblock_array, uint64_t x) {
+	if (bmblock_array->min > x || bmblock_array->max < x) {
+		return;
+	}
+	uint64_t relative_x = x - bmblock_array->min;
+	uint64_t mask = 1 << (relative_x % BM_MEMBER_SIZE);
+	mask = !mask;
+	bmblock_array->bm[relative_x / BM_MEMBER_SIZE] &= mask;
+	bmblock_array->cursor = bmblock_array->cursor > relative_x ? relative_x : bmblock_array->cursor;
 }
-#endif
+
+int bm_find_next(struct bmblock_array *bmblock_array) {
+	for (int i = bmblock_array->cursor / BM_MEMBER_SIZE ; i < bmblock_array->max/ BM_MEMBER_SIZE ; i++) {
+		if(bmblock_array->bm[i] != UINT64_C(-1)) {
+			for(int j = bmblock_array->cursor % BM_MEMBER_SIZE ; j < BM_MEMBER_SIZE ; j++) {
+				if (!((bmblock_array->bm[i] >> j) & 1)) {
+					bmblock_array->cursor = j + BM_MEMBER_SIZE * i;
+					return bmblock_array->cursor;
+				}
+			}
+		}
+		bmblock_array->cursor += BM_MEMBER_SIZE - (bmblock_array->cursor % BM_MEMBER_SIZE);
+	}
+	return ERR_BITMAP_FULL;
+}
+
+void bm_print(struct bmblock_array *bmblock_array) {
+	puts("**********BitMap Block START**********");
+	printf("length: %" PRIu64 "\n", bmblock_array->length);
+	printf("min: %" PRIu64 "\n", bmblock_array->min);
+	printf("max: %" PRIu64 "\n", bmblock_array->max);
+	printf("cursor: %" PRIu64 "\n", bmblock_array->cursor);
+	puts("content:");
+	for(int i = 0 ; i < bmblock_array->length ; i++) {
+		printf("%d:", i);
+		print_binary(bmblock_array->bm[i]);
+	}
+	puts("**********BitMap Block END************");
+}
+
+void print_binary(uint64_t bitmap_v) {
+	for(int i = 0 ; i < BM_MEMBER_SIZE ; i++) {
+		if (i % 8 == 0) {
+			printf(" ");
+		}
+		printf("%lu", (bitmap_v >> i) & 1);
+	}
+	puts("");
+}
